34d38842a419ce399097fe5eab48baf00130230e
polgasull <pgasulln@gmail.com>
2022-03-21 22:38:29 +0100

    Counter js empty

---
+++ src/components/Counter.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+ 
+function Counter() {
+  return (
+    <div className="Counter">
+      <h2>Counter</h2>
+ 
+      <p>You clicked 0 times</p>
+ 
+      <button> - </button>
+      <button> + </button>
+    </div>
+  );
+}
+ 
+export default Counter;
